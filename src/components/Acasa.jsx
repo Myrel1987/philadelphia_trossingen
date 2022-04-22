f5d0364c4dc959f5df1ad62db74451a81a441497
Apetrei <myrel1987@gmail.com>
2022-04-22 13:47:57 +0200

    Acasa page initiated

---
+++ src/components/Acasa.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Acasa() {
+  return (
+    <div>
+      <h1 className="w-full text-2xl font-bold text-[#00df9a]">
+        Bine ati venit
+      </h1>
+    </div>
+  );
+}
+
+export default Acasa;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
-      <h1 className="w-full text-3xl font-bold text-[#00df9a] ">
+      <h1 className="w-full text-2xl font-bold text-[#00df9a] ">
         Philadelphia Trossingen
       </h1>
-      <ul className="flex hidden">
+      <ul className="hidden md:flex">
         <li className="p-4">Acasã</li>
         <li className="p-4">Departamente</li>
         <li className="p-4">Resurse</li>
         <li className="p-4">Contact</li>
       </ul>
-      <div>
-        <AiOutlineMenu size={25} />
+      <div onClick={handleNav} className="block md:hidden">
+        {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
-      <div className="fixed left-0 top-0 w-[30%] h-full border-r border-r-gray-800 bg-gradient-to-r from-gray-900 to-gray-700">
+      <div
+        className={
+          !nav
+            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-800 bg-gradient-to-r from-gray-900 to-gray-700 ease-in-out duration-500"
+            : "fixed left-[-100%]"
+        }
+      >
         <h1 className="w-full text-3xl font-bold m-4 text-[#00c788] ">
           Philadelphia Trossingen
         </h1>
