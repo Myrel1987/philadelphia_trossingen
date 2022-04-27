f0770890e5f7606685c13aafcf61cbe0451be452
Apetrei <myrel1987@gmail.com>
2022-04-27 23:26:25 +0200

    some styles modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -9,9 +10,12 @@ const Navbar = () => {
   };
 
   return (
-    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+    <motion.div
+      Layout
+      className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white"
+    >
       <h1 className="w-full text-2xl font-bold text-[#00df9a] ">
-        Philadelphia Trossingen
+        Philadelphia
       </h1>
       <ul className="hidden md:flex">
         <li className="p-4">Acasã</li>
@@ -22,30 +26,25 @@ const Navbar = () => {
       <div onClick={handleNav} className="block md:hidden">
         {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
-      <div
+
+      <ul
         className={
-          !nav
+          nav
             ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-800 bg-gradient-to-r from-gray-900 to-gray-700 ease-in-out duration-500"
             : "fixed left-[-100%]"
         }
       >
         <h1 className="w-full text-3xl font-bold m-4 text-[#00c788] ">
-          Philadelphia Trossingen
+          Philadelphia-Trossingen
         </h1>
-        <ul className="uppercase p-4">
-          <li className="p-4 border-b border-gray-600 text-[#00df9a]">Acasã</li>
-          <li className="p-4 border-b border-gray-600 text-[#00df9a]">
-            Departamente
-          </li>
-          <li className="p-4 border-b border-gray-600 text-[#00df9a]">
-            Resurse
-          </li>
-          <li className="p-4 border-b border-gray-600 text-[#00df9a]">
-            Contact
-          </li>
-        </ul>
-      </div>
-    </div>
+        <li className="p-4 border-b border-gray-600 text-[#00df9a]">Acasã</li>
+        <li className="p-4 border-b border-gray-600 text-[#00df9a]">
+          Departamente
+        </li>
+        <li className="p-4 border-b border-gray-600 text-[#00df9a]">Resurse</li>
+        <li className="p-4 border-b border-gray-600 text-[#00df9a]">Contact</li>
+      </ul>
+    </motion.div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,8 @@
-const colors = require("tailwindcss/colors");
-
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      black: colors.black,
-      white: colors.white,
-      gray: colors.gray,
-      emerald: colors.emerald,
-      indigo: colors.indigo,
-      yellow: colors.yellow,
+      "regal-gray": "hsl(0, 0%, 30%)",
     },
   },
   plugins: [],
