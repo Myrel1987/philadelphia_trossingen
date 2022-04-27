ee787af3170c9cd46cfb6bc0c27eecc578b52a88
Apetrei <myrel1987@gmail.com>
2022-04-27 18:02:28 +0200

    framer-motion added

--- src/components/Acasa.jsx
+++ src/components/Acasa.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Acasa() {
   return (
     <div>
-      <h1 className="w-full text-2xl font-bold text-[#00df9a]">
+      <h1 className="w-full text-2xl font-bold text-[hsla(159, 64%, 61%, 0.5)]">
         Bine ati venit
       </h1>
     </div>
